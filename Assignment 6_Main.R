debb8dfbaad2fc0fe77f1638641ba5b6237dcaa3
mapg476 <mapg476@yahoo.com>
2024-03-02 12:35:53 -0800

    andrea changes

--- Assignment 6_Main.R
+++ Assignment 6_Main.R
@@ -59,3 +59,10 @@ ggplot(mean_fires_per_cause, aes(x = FIRE_CAUSE, y = mean_fires_per_year, fill =
   geom_boxplot(width = 2) +
   labs(x = "Fire Cause", y = "Mean Fires per Year") +
   theme_minimal()
+
+
+# hola
+# estamos
+# haciendo la
+# tarea
+# un sabado
